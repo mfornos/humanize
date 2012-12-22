4c725501c7535d0f99d7387f38ac755711221bad
Marc Fornós <marc.fornos@gmail.com>
2012-12-22 18:16:49 +0100

    Parsing, emoji chars and minor fixes

---
+++ humanize-slim/src/test/java/humanize/TestHumanizeParse.java
@@ -0,0 +1,102 @@
+package humanize;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestHumanizeParse {
+
+	private Locale defaultLocale;
+
+	@Test(threadPoolSize = 10, invocationCount = 10)
+	public void parseBase64() {
+
+		byte[] decoded = Humanize.parseBase64("R3JlZW4gb3ZlciBibGFjay4=");
+		String msg = new String(decoded);
+
+		Assert.assertEquals(msg, "Green over black.");
+	}
+
+	@Test(threadPoolSize = 10, invocationCount = 10)
+	public void parseDate() {
+
+		Date target = newDate(2011, 8, 14, 17, 22, 1);
+
+		Date date = Humanize.parseISODateTime("2011-09-14T15:22:01Z");
+		Assert.assertEquals(date, target);
+
+		date = Humanize.parseISODate("2011-09-14T15:22:01Z");
+		Assert.assertEquals(date, target);
+
+		target = newTime(16, 22, 1);
+		date = Humanize.parseISOTime("15:22:01Z");
+		Assert.assertEquals(date, target);
+
+	}
+
+	@Test(threadPoolSize = 10, invocationCount = 10)
+	public void parseSmartDate() {
+
+		Date target = newDate(2012, 1, 1, 0, 0, 0);
+
+		String dates[] = new String[] { "1.2.12", "01.02.2012", "2012.02.01", "01-02-12", "1 2 2012" };
+
+		for (String ds : dates) {
+			Date date = Humanize.parseSmartDate(ds, "dd/MM/yy", "yyyy/MM/dd", "dd/MM/yyyy");
+			Assert.assertEquals(date, target);
+		}
+
+		for (String ds : dates) {
+			Date date = Humanize.parseSmartDate(ds, "dd/MM/yy,yyyy/MM/dd,dd/MM/yyyy".split(","));
+			Assert.assertEquals(date, target);
+		}
+
+		for (String ds : "1$2$12,01$02$2012,2012$02$01".split(",")) {
+			Date date = Humanize.parseSmartDateWithSeparator(ds, "\\$+", "dd/MM/yy,yyyy/MM/dd,dd/MM/yyyy".split(","));
+			Assert.assertEquals(date, target);
+		}
+
+	}
+
+	@AfterClass
+	void after() {
+
+		Locale.setDefault(defaultLocale);
+
+	}
+
+	@BeforeClass
+	void before() {
+
+		defaultLocale = Locale.getDefault();
+		Locale.setDefault(Locale.ENGLISH);
+
+	}
+
+	private Date newDate(int year, int month, int day, int hour, int minute, int second) {
+
+		Calendar cal = Calendar.getInstance();
+		cal.set(year, month, day, hour, minute, second);
+		cal.set(Calendar.MILLISECOND, 0);
+		return cal.getTime();
+
+	}
+
+	private Date newTime(int hour, int minute, int second) {
+
+		Calendar cal = Calendar.getInstance();
+		cal = Calendar.getInstance();
+		cal.setTimeInMillis(0);
+		cal.set(Calendar.HOUR, hour);
+		cal.set(Calendar.MINUTE, minute);
+		cal.set(Calendar.SECOND, second);
+		return cal.getTime();
+
+	}
+
+}
