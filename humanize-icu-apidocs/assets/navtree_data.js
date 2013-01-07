2849391f33289f4c7261d41aa6ff087c7045230a
Marc Fornós <marc.fornos@gmail.com>
2013-01-07 21:44:08 +0100

    generated javadoc

---
+++ humanize-icu-apidocs/assets/navtree_data.js
@@ -0,0 +1,12 @@
+var NAVTREE_DATA =
+[ [ "humanize", "humanize/package-summary.html", [ [ "Classes", null, [ [ "ICUHumanize", "humanize/ICUHumanize.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "humanize.icu.spi", "humanize/icu/spi/package-summary.html", [ [ "Classes", null, [ [ "MessageFormat", "humanize/icu/spi/MessageFormat.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "humanize.icu.spi.context", "humanize/icu/spi/context/package-summary.html", [ [ "Interfaces", null, [ [ "ICUContext", "humanize/icu/spi/context/ICUContext.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "DefaultICUContext", "humanize/icu/spi/context/DefaultICUContext.html", null, "" ], [ "ICUContextFactory", "humanize/icu/spi/context/ICUContextFactory.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ humanize-icu-apidocs/reference/lists.js
@@ -0,0 +1,11 @@
+var DATA = [
+      { id:0, label:"humanize", link:"humanize/package-summary.html", type:"package" },
+      { id:1, label:"humanize.ICUHumanize", link:"humanize/ICUHumanize.html", type:"class" },
+      { id:2, label:"humanize.icu.spi", link:"humanize/icu/spi/package-summary.html", type:"package" },
+      { id:3, label:"humanize.icu.spi.MessageFormat", link:"humanize/icu/spi/MessageFormat.html", type:"class" },
+      { id:4, label:"humanize.icu.spi.context", link:"humanize/icu/spi/context/package-summary.html", type:"package" },
+      { id:5, label:"humanize.icu.spi.context.DefaultICUContext", link:"humanize/icu/spi/context/DefaultICUContext.html", type:"class" },
+      { id:6, label:"humanize.icu.spi.context.ICUContext", link:"humanize/icu/spi/context/ICUContext.html", type:"class" },
+      { id:7, label:"humanize.icu.spi.context.ICUContextFactory", link:"humanize/icu/spi/context/ICUContextFactory.html", type:"class" }
+
+    ];
